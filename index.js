39f94d5b59e3eacc393af076e5bb377675ee573c
Philip Scott <pscott@zeptohost.com>
2018-08-20 07:16:21 -0400

    refactor: Remove spawn process (#1)
    BREAKING CHANGE: Removed child_process method option

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const JSONStream = require('JSONStream')
-const bl = require('bl')
-const { spawn, execFile } = require('child_process')
+const { execFile } = require('child_process')
 
 function ffprobeExecFile (path, params) {
   return new Promise((resolve, reject) => {
@@ -19,40 +17,8 @@ function ffprobeExecFile (path, params) {
   })
 }
 
-function ffprobeSpawn (path, params) {
-  let stdout
-  let stderr
-  const ffprobe = spawn(path, params)
-
-  ffprobe.stdout
-    .pipe(JSONStream.parse())
-    .once('data', (data) => {
-      stdout = data
-    })
-
-  return new Promise((resolve, reject) => {
-    ffprobe.stderr
-      .pipe(bl((err, data) => {
-        if (err) reject(err)
-
-        stderr = data.toString()
-      }))
-
-    ffprobe.once('close', (exitCode) => {
-      if (exitCode === 0) {
-        resolve(stdout)
-      } else {
-        // Relevant ffprobe error is last line
-        const ffprobeErr = new Error(stderr.split('\n').pop())
-        reject(ffprobeErr)
-      }
-    })
-  })
-}
-
-function ffprobeExec (target, config = {}) {
+function ffprobe (target, config = {}) {
   const path = config.path || 'ffprobe'
-  const method = config.method || 'execFile'
   const params = [
     '-show_streams',
     '-show_format',
@@ -61,15 +27,7 @@ function ffprobeExec (target, config = {}) {
     target
   ]
 
-  switch (method) {
-    case 'execFile':
-      return ffprobeExecFile(path, params)
-    case 'spawn':
-      return ffprobeSpawn(path, params)
-    default:
-      const err = new Error('Invalid method configuration; please use spawn or execFile')
-      return Promise.reject(err)
-  }
+  return ffprobeExecFile(path, params)
 }
 
-module.exports = ffprobeExec
+module.exports = ffprobe
