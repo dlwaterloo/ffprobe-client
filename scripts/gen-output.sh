d0d861c44354bf847f3c8095714af0598887666a
Philip Scott <pscott@zeptohost.com>
2018-08-24 11:02:38 -0400

    add echos

--- scripts/gen-output.sh
+++ scripts/gen-output.sh
@@ -5,7 +5,11 @@ URL="https://www.w3schools.com/html/mov_bbb.webm"
 FILE="./test/data/input.webm"
 OUT="./test/data"
 
+echo "Generating ffprobe output!"
+
 ffprobe $ARGS $FILE > "$OUT/local-file-output.json" 2> /dev/null
 ffprobe $ARGS $URL > "$OUT/local-url-output.json" 2> /dev/null
 ./test/bin/linux/x64/ffprobe $ARGS $FILE > "$OUT/static-file-output.json" 2> /dev/null
 ./test/bin/linux/x64/ffprobe $ARGS $URL > "$OUT/static-url-output.json" 2> /dev/null
+
+echo "Finished generating ffprobe output!"
