cb973d5975870d1e32012e1f39ab4b94aa491921
Philip Scott <pscott@zeptohost.com>
2018-08-23 20:06:22 -0400

    test: Add tests and binary (#2)
    * test: Add tests and binary
    
    * docs: Update package.json

---
+++ scripts/gen-output.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+
+ARGS="-show_streams -show_format -print_format json"
+URL="https://www.w3schools.com/html/mov_bbb.webm"
+FILE="./test/data/input.webm"
+OUT="./test/data"
+
+ffprobe $ARGS $FILE > "$OUT/local-file-output.json" 2> /dev/null
+ffprobe $ARGS $URL > "$OUT/local-url-output.json" 2> /dev/null
+./test/bin/linux/x64/ffprobe $ARGS $FILE > "$OUT/static-file-output.json" 2> /dev/null
+./test/bin/linux/x64/ffprobe $ARGS $URL > "$OUT/static-url-output.json" 2> /dev/null
